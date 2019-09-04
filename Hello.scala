867d116485b74edd2dd4a36a700d2da354b7d366
anagaraju <nagaraju.48458@gmail.com>
2019-09-04 15:45:38 +0530

    branch changes

--- Hello.scala
+++ Hello.scala
@@ -1,7 +1,7 @@
 object Hello {
     def main(args: Array[String]) {
         println("Hello, world")
-         println("brach changes")
+         println("brach changessssssssssss")
         println("master changes")
 
     }
